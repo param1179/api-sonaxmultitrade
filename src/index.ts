6bc1c7b916415482656ffa388e5843539986344b
Paramjeet Singh <paramsingh1179@gmail.com>
2022-12-15 17:59:30 +0530

    updated

--- src/index.ts
+++ src/index.ts
@@ -22,9 +22,9 @@ app.use("/api", routes);
 
 app.use(ErrorHandler);
 
-app.use(express.static(path.resolve(__dirname, "../../kids-admin/build")));
+app.use(express.static(path.resolve(__dirname, "../../web-sonaxmultitrade/build")));
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../../kids-admin/build", "index.html"));
+  res.sendFile(path.resolve(__dirname, "../../web-sonaxmultitrade/build", "index.html"));
 });
 
 app.listen(PORT, () => {
